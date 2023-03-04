150aba8e739122184f1ce10d5cae732677bf9adf
Hunter <realisvenator@gmail.com>
2023-03-04 13:04:27 -0600

    for sure last main commit

--- client/src/components/Ship.jsx
+++ client/src/components/Ship.jsx
@@ -1,7 +1,16 @@
 const Ship = () => {
     const parse = (ship) =>{
-        let input = JSON.parse(ship);
-        console.log(input);
+        let shipData = JSON.parse(ship);
+        console.log(shipData);
+        let name = shipData.AssemblyDefinition.assemblyName;
+        let description = shipData.AssemblyDefinition.description;
+        let version = shipData.AssemblyDefinition.version;  
+        let numStages = shipData.AssemblyOABConfig.StagingState.totalStageCount;
+        console.log(name);
+        console.log(description);
+        console.log(version);
+        console.log(numStages);
+
     }
 
     return (
